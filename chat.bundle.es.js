59d9baf9a4420015e5e0199db714ad4b48c54402
kpjoel <71575933+kpjoel@users.noreply.github.com>
2025-07-01 14:16:03 +0100

    Update chat.bundle.es.js

--- chat.bundle.es.js
+++ chat.bundle.es.js
@@ -296,8 +296,8 @@
             welcomeText: '',
             responseTimeText: '',
             poweredBy: {
-                text: 'Powered by n8n',
-                link: 'https://n8n.partnerlinks.io/m8a94i19zhqq?utm_source=nocodecreative.io'
+                text: 'Powered by Tiqva',
+                link: 'https://tiqva.uk'
             }
         },
         style: {
